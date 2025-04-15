c3d1d7229eeea279f4310bc567717dcd68134b22
John Doe <miromaxxs@gmail.com>
2025-04-15 20:55:48 +0300

    add test var full

--- z_test.go
+++ z_test.go
@@ -4,152 +4,149 @@ import (
 	"fmt"
 	"net"
 	"reflect"
+	"strings"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/require"
 )
 
+const name = "sample.name"
+
 type anyFn[T any] func(string, T, string, ...OptNode) *T
 
 func regSource[T any](fn anyFn[T], v T) (reg func() any, value any, source map[string]string) {
 	var def T
 	reg = func() any {
-		return fn("name", def, "desc")
+		return fn(name, def, "desc")
 	}
 
-	source = map[string]string{"name": ToString(v)}
+	source = map[string]string{name: ToString(v)}
 
 	return reg, v, source
 }
 
 func Test_ValueOk(t *testing.T) {
 	tests := []struct {
-		name string
-		init func() (ptr func() any, val any, src map[string]string)
+		varType string
+		init    func() (ptr func() any, val any, src map[string]string)
 	}{
 		{
-			name: "default",
-			init: func() (func() any, any, map[string]string) {
-				return regSource(Int, 0)
-			},
-		},
-		{
-			name: "int",
+			varType: "int",
 			init: func() (func() any, any, map[string]string) {
 				return regSource(Int, 5)
 			},
 		},
 		{
-			name: "uint",
+			varType: "uint",
 			init: func() (func() any, any, map[string]string) {
 				return regSource(Uint, uint(42))
 			},
 		},
 		{
-			name: "int32",
+			varType: "int32",
 			init: func() (func() any, any, map[string]string) {
 				return regSource(Int32, int32(123))
 			},
 		},
 		{
-			name: "uint32",
+			varType: "uint32",
 			init: func() (func() any, any, map[string]string) {
 				return regSource(Uint32, uint32(456))
 			},
 		},
 		{
-			name: "int64",
+			varType: "int64",
 			init: func() (func() any, any, map[string]string) {
 				return regSource(Int64, int64(789))
 			},
 		},
 		{
-			name: "uint64",
+			varType: "uint64",
 			init: func() (func() any, any, map[string]string) {
 				return regSource(Uint64, uint64(1011))
 			},
 		},
 		{
-			name: "true bool",
+			varType: "bool true",
 			init: func() (func() any, any, map[string]string) {
 				return regSource(Bool, true)
 			},
 		},
 		{
-			name: "false bool",
+			varType: "bool false",
 			init: func() (func() any, any, map[string]string) {
 				return regSource(Bool, false)
 			},
 		},
 		{
-			name: "bools",
+			varType: "bools",
 			init: func() (func() any, any, map[string]string) {
 				return regSource(Bools, []bool{true, false, true})
 			},
 		},
 		{
-			name: "ints",
+			varType: "ints",
 			init: func() (func() any, any, map[string]string) {
 				return regSource(Ints, []int{1, 2, 3})
 			},
 		},
 		{
-			name: "str",
+			varType: "string",
 			init: func() (func() any, any, map[string]string) {
 				return regSource(Str, "value")
 			},
 		},
 		{
-			name: "strs",
+			varType: "strings",
 			init: func() (func() any, any, map[string]string) {
 				return regSource(Strs, []string{"a", "b", "c"})
 			},
 		},
 		{
-			name: "float32",
+			varType: "float32",
 			init: func() (func() any, any, map[string]string) {
 				return regSource(Float32, float32(3.14))
 			},
 		},
 		{
-			name: "float32s",
+			varType: "float32s",
 			init: func() (func() any, any, map[string]string) {
 				return regSource(Floats32, []float32{1.1, 2.2, 3.3})
 			},
 		},
 		{
-			name: "float64",
+			varType: "float64",
 			init: func() (func() any, any, map[string]string) {
 				return regSource(Float64, 3.14159265359)
 			},
 		},
 		{
-			name: "float64s",
+			varType: "float64s",
 			init: func() (func() any, any, map[string]string) {
 				return regSource(Floats64, []float64{1.1, 2.2, 3.3})
 			},
 		},
 		{
-			name: "duration",
+			varType: "duration",
 			init: func() (func() any, any, map[string]string) {
 				return regSource(Dur, 5*time.Second)
 			},
 		},
 		{
-			name: "durations",
+			varType: "durations",
 			init: func() (func() any, any, map[string]string) {
 				return regSource(Durs, []time.Duration{time.Second, 2 * time.Minute, 3 * time.Hour})
 			},
 		},
 		{
-			name: "ip",
+			varType: "ip",
 			init: func() (func() any, any, map[string]string) {
 				return regSource(ipInternal, net.ParseIP("192.168.1.1"))
 			},
 		},
 		{
-			name: "map",
+			varType: "map",
 			init: func() (func() any, any, map[string]string) {
 				return regSource(mapInternal, map[string]any{"float": 1., "str": "val"})
 			},
@@ -165,7 +162,7 @@ func Test_ValueOk(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
+		t.Run(tt.varType, func(t *testing.T) {
 			c = defaultConfig()
 
 			reg, expected, source := tt.init()
@@ -178,6 +175,25 @@ func Test_ValueOk(t *testing.T) {
 			require.NoError(t, err)
 
 			require.EqualValues(t, expected, actual)
+
+			// check string representation <-> sources map[string]string
+			node, ok := c.vs[name]
+			require.True(t, ok)
+
+			newValue := node.Value.String()
+			source[name] = newValue
+
+			err = c.applyParser(source)
+			require.NoError(t, err)
+
+			updatedNode, ok := c.vs[name]
+			require.True(t, ok)
+
+			require.Equal(t, newValue, updatedNode.Value.String())
+
+			// check type name
+			awaitedType := strings.Split(tt.varType, " ")[0]
+			require.Equal(t, awaitedType, updatedNode.Value.Type())
 		})
 	}
 }
